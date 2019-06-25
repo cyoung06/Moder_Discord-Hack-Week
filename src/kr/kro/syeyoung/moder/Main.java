aab261e59e9ab8f4181a6a7e16ac4a638aaf6177
syeyoung <cyoung06@naver.com>
2019-06-25 13:15:30 -0400

    Update Main.java

--- src/kr/kro/syeyoung/moder/Main.java
+++ src/kr/kro/syeyoung/moder/Main.java
@@ -1,5 +1,10 @@
 package kr.kro.syeyoung.moder;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.core.AccountType;
@@ -12,8 +17,24 @@ import net.dv8tion.jda.core.entities.Game.GameType;
 public class Main {
 	
 	private JDA jda;
+
+	private static Properties configuration;
+
+	static {
+		configuration = new Properties();
+		FileInputStream fis = null;
+		try {
+			configuration.load(fis);
+			fis = new FileInputStream("config.properties");
+			fis.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
-	private final static String token = "";
+	private final static String token = configuration.getProperty("token");
+	
+	
 	
 	public Main() throws LoginException, InterruptedException {
 		jda = new JDABuilder(AccountType.BOT).setToken(token).setStatus(OnlineStatus.ONLINE).setGame(Game.of(GameType.LISTENING, "+help")).build();
